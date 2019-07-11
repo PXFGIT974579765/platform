2b85dcc3041512936d21f6bfe1ecbbf662874735
evantre <evantre@qq.com>
2019-07-11 23:55:30 +0800

    feat: errand/order

--- src/views/Errand/Order.vue
+++ src/views/Errand/Order.vue
@@ -51,7 +51,7 @@
         <router-link to="/news" class="block-header-link">收费标准</router-link>
       </div>
       <div class="prices">
-        <button>2元</button>
+        <button class="active">2元</button>
         <button>3元</button>
         <button>4元</button>
         <button>自定义</button>
@@ -59,12 +59,24 @@
     </div>
 
     <div class="pay-methods block">
-      <span></span>
-      <span>微信支付</span>
-      <van-checkbox checked-color="#07c160" />
+      <van-radio-group v-model="radio">
+        <div class="pay-method weixin">
+          <span class="iconfont">&#xe758;</span>
+          <div class="name">微信支付</div>
+          <van-radio :name="1" checked-color="#07c160" />
+        </div>
+        <div class="pay-method account">
+          <span class="iconfont">&#xe777;</span>
+          <div class="name">
+            余额
+            <span>可用余额: 89.80元</span>
+          </div>
+          <van-radio :name="2" checked-color="#07c160" />
+        </div>
+      </van-radio-group>
     </div>
 
-    <div class="pay block">
+    <div class="pay">
       <div class="price">
         支付
         <span>￥2.00元</span>
@@ -92,6 +104,7 @@ export default {
         score: 9.2,
       },
       comment: '',
+      radio: 1,
     }
   },
 }
@@ -164,6 +177,9 @@ export default {
     color: #fff;
     background-color: #029eff;
   }
+  &.address-put .type {
+    background-color: #1cd547;
+  }
   .detail {
     flex: 1;
   }
@@ -183,6 +199,7 @@ export default {
     border-bottom: 1px solid #c8c8c8;
   }
   .button {
+    margin-top: 0.1em;
     padding-left: 15px;
     font-size: 13px;
     color: #029eff;
@@ -205,8 +222,81 @@ export default {
 }
 
 .cost {
+  .prices {
+    display: flex;
+    margin-top: 20px;
+  }
+  button {
+    flex: 1;
+    height: 30px;
+    line-height: 30px;
+    margin-right: 9px;
+    padding: 0;
+    border-radius: 3px;
+    font-size: 13px;
+    border: solid 1px #b4b4b4;
+    color: #585858;
+    &:last-child {
+      margin: 0;
+    }
+    &.active {
+      border-color: #00b034;
+      color: #00b034;
+    }
+  }
 }
 
-.pay-methods {
+.pay-method {
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid #c8c8c8;
+  &:last-child {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+  .name {
+    flex: 1;
+    margin: 0 11px;
+    color: #585858;
+    span {
+      margin-left: 12px;
+      font-size: 13px;
+      color: #a4a4a4;
+    }
+  }
+  &.weixin .iconfont {
+    color: #00b700;
+  }
+  &.account .iconfont {
+    color: #ffc000;
+  }
+}
+
+.pay {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin: 84px -15px 76px;
+  background: #ffffff;
+  > * {
+    flex: 1;
+  }
+  .price {
+    font-size: 12px;
+    color: #585858;
+    span {
+      font-size: 15px;
+      color: #ff4545;
+    }
+  }
+  button {
+    height: 48px;
+    line-height: 48px;
+    color: #fff;
+    background: #0cc3b1;
+  }
 }
 </style>
