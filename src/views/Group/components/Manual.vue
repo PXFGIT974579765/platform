b04c7dd059857e791fefcd42e879f9da0016bdbc
evantre <evantre@qq.com>
2019-07-13 21:01:40 +0800

    feat: group/detail

---
+++ src/views/Group/components/Manual.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="manual">
+    <div class="title">拼团玩法</div>
+    <div class="content">
+      <span>选择商品</span>
+      <div class="sep">></div>
+      <span>邀请好友参团</span>
+      <div class="sep">></div>
+      <span>人数达到最低要求即拼团成功</span>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.manual {
+  display: flex;
+  align-items: center;
+  margin: 8px 0;
+  padding: 15px;
+  line-height: 1;
+  background: #fff;
+}
+
+.content {
+  flex: 1;
+  display: flex;
+  margin-left: 14px;
+  line-height: 1.2;
+  font-size: 10px;
+}
+
+.sep {
+  margin: 0 5px;
+}
+</style>
