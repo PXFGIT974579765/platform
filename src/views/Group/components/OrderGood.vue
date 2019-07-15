ac5938604bad71817935fa3d27290d45e5d95b2e
evantre <evantre@qq.com>
2019-07-15 22:30:56 +0800

    feat: group order

---
+++ src/views/Group/components/OrderGood.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="order-good">
+    <div class="title">拼团订单</div>
+
+    <div class="good">
+      <img src="~@/assets/images/group_good.png" alt />
+
+      <div class="detail">
+        <div class="name">新款时尚简约休闲小西服阔腿短裤韩国两件</div>
+
+        <div class="style">
+          <div class="color">蓝色</div>
+          <div class="size">M号</div>
+        </div>
+
+        <div class="buy">
+          <div class="price">￥235.00</div>
+          <div class="count">X{{ value }}</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="num">
+      <div class="name">购买数量</div>
+      <van-stepper v-model="value" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 1,
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.order-good {
+  margin: 8px 0;
+}
+
+.good {
+  display: flex;
+  padding: 15px;
+
+  img {
+    width: 100px;
+    height: 100px;
+    margin-right: 10px;
+  }
+
+  .name {
+    line-height: 1.4;
+    font-size: 16px;
+    color: #4e4e4e;
+  }
+
+  .style {
+    display: flex;
+    margin: 10px 0 15px;
+    font-size: 13px;
+    color: #ababab;
+  }
+
+  .color {
+    margin-right: 15px;
+  }
+
+  .buy {
+    display: flex;
+    align-items: center;
+  }
+
+  .price {
+    flex: 1;
+    font-size: 16px;
+    color: #232323;
+  }
+
+  .count {
+    font-size: 13px;
+    color: #6e6e6e;
+  }
+}
+
+.title,
+.num {
+  height: 42px;
+  line-height: 42px;
+  padding: 0 15px;
+  color: #2d2d2d;
+  background: #fff;
+}
+
+.num {
+  display: flex;
+
+  .name {
+    flex: 1;
+  }
+}
+</style>
---
+++ src/views/Group/components/OrderSubmit.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="order-submit">
+    <div class="price">
+      支付
+      <span>￥2.00元</span>
+    </div>
+    <button @click="onSubmit">提交订单</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmit() {
+      //
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.order-submit {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin: 84px -15px 76px;
+  background: #ffffff;
+  > * {
+    flex: 1;
+  }
+  .price {
+    font-size: 12px;
+    color: #585858;
+    span {
+      font-size: 15px;
+      color: #ff4545;
+    }
+  }
+  button {
+    height: 48px;
+    line-height: 48px;
+    color: #fff;
+    background: #0cc3b1;
+  }
+}
+</style>
