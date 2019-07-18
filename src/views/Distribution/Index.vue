d98cb8e0019e90cbf456672d7564c8704a082b30
peng xiongfeng <pengxiongfeng@cetcbigdata.com>
2019-07-18 23:05:26 +0800

    feat: page distribution onlining

--- src/views/Distribution/Index.vue
+++ src/views/Distribution/Index.vue
@@ -1,10 +1,39 @@
 <template>
   <div class="page-my-distribution" v-wechat-title="$route.meta.title">
     <div class="info-card">
-      <img src="./images/avator2.png" class="avator" alt="" />
+      <img src="./images/avator2.png" class="avator" alt />
       <div class="nickname">Hi {{ info.nickname }}</div>
       <div class="title">今天又是美好的一天，加油哦！</div>
-      <div class="btn-online" @click="routeCondition">上线接单</div>
+      <div v-if="info.status === 0" class="btn-online" @click="routeCondition">
+        上线接单
+      </div>
+      <div v-else>
+        <div class="condition-area">
+          <div class="condition flex">
+            <div>
+              <span class="iconfont">&#xe74f;</span>
+              <span>贵州大学城贵州师范大学***B2栋</span>
+            </div>
+          </div>
+          <div class="condition flex">
+            <div>
+              <span class="iconfont">&#xe74c;</span>
+              <span>今天 12:00 - 14:00</span>
+            </div>
+            <div class="btn-switch-block flex">
+              提前下线
+              <van-switch
+                v-model="checked"
+                class="btn-switch"
+                active-color="#06bcbf"
+                inactive-color="#eee"
+                size="20px"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="status">接单中</div>
+      </div>
     </div>
     <div class="btn-my-order">我的配送订单</div>
     <div class="footer">
@@ -19,7 +48,9 @@
 export default {
   data() {
     return {
+      checked: true,
       info: {
+        status: 1, // 0 未开始接单   1 接单中
         nickname: '小刘哥',
       },
     }
@@ -72,6 +103,40 @@ export default {
       border-radius: 20px;
       color: #fff;
       background-color: #07c2af;
+    }
+
+    .condition-area {
+      margin-top: 22px;
+      padding: 29px 19px;
+      background-color: #eee;
+
+      .condition {
+        margin-top: 21px;
+        align-items: center;
+        justify-content: space-between;
+
+        .iconfont {
+          width: 11px;
+          height: 13px;
+          margin-right: 9px;
+          color: #32b16c;
+        }
+
+        .btn-switch-block {
+          align-items: center;
+
+          .btn-switch {
+            margin-left: 8px;
+          }
+        }
+      }
+    }
+
+    .status {
+      height: 60px;
+      line-height: 60px;
+      font-size: 15px;
+      color: #434343;
     }
   }
 
