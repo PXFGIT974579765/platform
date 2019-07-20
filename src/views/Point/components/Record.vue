4fcc455ae144763a8af8a0796e4daded64581ffa
evantre <evantre@qq.com>
2019-07-20 23:48:43 +0800

    feat: point/index

--- src/views/Point/components/Record.vue
+++ src/views/Point/components/Record.vue
@@ -1,9 +1,111 @@
 <template>
-  <div>test</div>
+  <div class="record">
+    <div class="item">
+      <img src="~@/assets/images/group_item.png" alt />
+      <div class="content">
+        <div class="name">
+          保温杯水杯定制广告杯刻字印logo活动礼品茶杯子开业赠品纪念定做
+        </div>
+        <div class="status success">兑换成功</div>
+        <div class="others">
+          <div class="date">2019.06.19 12:25</div>
+          <div class="count">-1000积分</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="~@/assets/images/group_item.png" alt />
+      <div class="content">
+        <div class="name">
+          保温杯水杯定制广告杯刻字印logo活动礼品茶杯子开业赠品纪念定做
+        </div>
+        <div class="status success">兑换成功</div>
+        <div class="others">
+          <div class="date">2019.06.19 12:25</div>
+          <div class="count">-1000积分</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="~@/assets/images/group_item.png" alt />
+      <div class="content">
+        <div class="name">
+          保温杯水杯定制广告杯刻字印logo活动礼品茶杯子开业赠品纪念定做
+        </div>
+        <div class="status success">兑换成功</div>
+        <div class="others">
+          <div class="date">2019.06.19 12:25</div>
+          <div class="count">-1000积分</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="~@/assets/images/group_item.png" alt />
+      <div class="content">
+        <div class="name">
+          保温杯水杯定制广告杯刻字印logo活动礼品茶杯子开业赠品纪念定做
+        </div>
+        <div class="status success">兑换成功</div>
+        <div class="others">
+          <div class="date">2019.06.19 12:25</div>
+          <div class="count">-1000积分</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.item {
+  display: flex;
+  margin-bottom: 10px;
+  padding: 22px 15px;
+  font-size: 15px;
+  background: #fff;
+}
+
+img {
+  width: 92px;
+  height: 92px;
+  margin-right: 13px;
+  border-radius: 4px;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.name {
+  line-height: 1.5;
+  color: #000;
+}
+
+.status {
+  &.success {
+    color: #07c2af;
+  }
+}
+
+.others {
+  display: flex;
+}
+
+.date {
+  flex: 1;
+  font-size: 13px;
+  color: #a4a4a4;
+}
+
+.count {
+  color: #fe5454;
+}
+</style>
