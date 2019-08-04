f329672c539d4acd59362473f36894d3e452fa3e
evantre <evantre@qq.com>
2019-08-04 16:21:23 +0800

    feat: api app

--- src/views/Accept/index.vue
+++ src/views/Accept/index.vue
@@ -8,22 +8,23 @@ import { mapActions } from 'vuex'
 export default {
   methods: mapActions(['setUser', 'setAccess']),
 
-  created() {
-    const { code } = this.$route.query
-    this.$http
-      .post('/api-wxmp/wxmp-anon/client/credentials', {
-        code: code,
-      })
-      .then(({ data }) => {
-        if (data.resp_code !== 0) {
-          this.$router.push('/')
-          return
-        }
-        const { accessToken, userInfo } = data.datas
-        this.setUser(userInfo)
-        this.setAccess(accessToken)
-        this.$router.push('/index/home')
-      })
-  },
+  // TODO: remove comment
+  // created() {
+  //   const { code } = this.$route.query
+  //   this.$http
+  //     .post('/api-wxmp/wxmp-anon/client/credentials', {
+  //       code: code,
+  //     })
+  //     .then(({ data }) => {
+  //       if (data.resp_code !== 0) {
+  //         this.$router.push('/')
+  //         return
+  //       }
+  //       const { accessToken, userInfo } = data.datas
+  //       this.setUser(userInfo)
+  //       this.setAccess(accessToken)
+  //       this.$router.push('/index/home')
+  //     })
+  // },
 }
 </script>
