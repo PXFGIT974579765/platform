1948b3e76e1df7d2a506b64c0a0479deb9e85202
peng xiongfeng <pengxiongfeng@cetcbigdata.com>
2019-08-04 15:13:53 +0800

    feat: complain

--- src/views/Accept/index.vue
+++ src/views/Accept/index.vue
@@ -9,21 +9,21 @@ export default {
   methods: mapActions(['setUser', 'setAccess']),
 
   created() {
-    const { code } = this.$route.query
-    this.$http
-      .post('/api-wxmp/wxmp-anon/client/credentials', {
-        code: code,
-      })
-      .then(({ data }) => {
-        if (data.resp_code !== 0) {
-          this.$router.push('/')
-          return
-        }
-        const { accessToken, userInfo } = data.datas
-        this.setUser(userInfo)
-        this.setAccess(accessToken)
-        this.$router.push('/index/home')
-      })
+    // const { code } = this.$route.query
+    // this.$http
+    //   .post('/api-wxmp/wxmp-anon/client/credentials', {
+    //     code: code,
+    //   })
+    //   .then(({ data }) => {
+    //     if (data.resp_code !== 0) {
+    //       this.$router.push('/')
+    //       return
+    //     }
+    //     const { accessToken, userInfo } = data.datas
+    //     this.setUser(userInfo)
+    //     this.setAccess(accessToken)
+    //     this.$router.push('/index/home')
+    //   })
   },
 }
 </script>
