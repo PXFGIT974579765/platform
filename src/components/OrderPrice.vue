e2c05e72381e83274f60c9b90776a383e9144213
evantre <evantre@qq.com>
2019-08-11 15:01:17 +0800

    feat: pay

--- src/components/OrderPrice.vue
+++ src/components/OrderPrice.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="order-price">
-    <div class="item">
+    <div class="item" @click="onSelect">
       <div class="item-name">优惠券</div>
       <div class="tag">下单立减10元</div>
       <div class="item-value ticket">
@@ -12,6 +12,45 @@
       <div class="item-name">订单总价</div>
       <div class="item-value price">￥{{ order.price }}</div>
     </div>
+
+    <van-dialog
+      v-model="ticketShow"
+      :showConfirmButton="false"
+      closeOnPopstate
+      closeOnClickOverlay
+    >
+      <div class="ticket-select">
+        <van-icon name="cross" :size="16" class="close" @click="onClose" />
+        <div class="ticket-list">
+          <van-radio-group v-model="radio">
+            <div class="ticket-item">
+              <van-radio :name="2" checked-color="#07c160" />
+              <div class="ticket-detail">
+                <div class="ticket-name">
+                  贵阳市花溪大学城贵州师范大学科创园B5栋负一层
+                </div>
+                <div class="contact">
+                  白小姐
+                  <span class="phone">19985501144</span>
+                </div>
+              </div>
+            </div>
+            <div class="ticket-item">
+              <van-radio :name="1" checked-color="#07c160" />
+              <div class="ticket-detail">
+                <div class="ticket-name">
+                  贵州财经大学（大学城校区）A3栋一楼12号
+                </div>
+                <div class="contact">
+                  李小姐
+                  <span class="phone">18085070709</span>
+                </div>
+              </div>
+            </div>
+          </van-radio-group>
+        </div>
+      </div>
+    </van-dialog>
   </div>
 </template>
 
@@ -21,6 +60,23 @@ export default {
     order: {
       type: Object,
       default: () => ({}),
+    },
+  },
+
+  data() {
+    return {
+      radio: 1,
+      ticketShow: false,
+    }
+  },
+
+  methods: {
+    onSelect() {
+      this.ticketShow = true
+    },
+
+    onClose() {
+      this.ticketShow = false
     },
   },
 }
@@ -62,5 +118,47 @@ export default {
 .price {
   font-size: 16px;
   color: #ff4a4a;
+}
+
+.ticket-select {
+  position: relative;
+  padding: 40px 17px;
+
+  .close {
+    position: absolute;
+    top: 12px;
+    right: 12px;
+    color: #adadad;
+    cursor: pointer;
+  }
+}
+
+.ticket-list {
+  max-height: 60vh;
+  overflow: auto;
+}
+
+.ticket-item {
+  display: flex;
+  margin-bottom: 20px;
+  padding-bottom: 20px;
+  border-bottom: solid 1px #e5e5e5;
+
+  &:last-child {
+    margin-bottom: 0;
+    padding-bottom: 0;
+    border: 0;
+  }
+}
+
+.ticket-detail {
+  flex: 1;
+  margin-left: 8px;
+  line-height: 1.5;
+  color: #4f4f4f;
+}
+
+.ticket-name {
+  margin-bottom: 10px;
 }
 </style>
--- src/views/Activity/Order.vue
+++ src/views/Activity/Order.vue
@@ -91,13 +91,17 @@ export default {
     },
 
     pay(opts) {
-      console.log('----', opts)
-      WeixinJSBridge.invoke('getBrandWCPayRequest', opts, function(res) {
-        console.log(res)
-        if (res.err_msg == 'get_brand_wcpay_request:ok') {
-          // 使用以上方式判断前端返回,微信团队郑重提示：
-          //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+      WeixinJSBridge.invoke('getBrandWCPayRequest', opts, res => {
+        if (res.err_msg === 'get_brand_wcpay_request:ok') {
+          this.$router.push('/order/active')
+          return
         }
+        // if (res.err_msg === 'get_brand_wcpay_request:cancel') {
+        //   // return
+        // }
+        // if (res.err_msg === 'get_brand_wcpay_request:fail') {
+        //   // return
+        // }
       })
     },
   },
