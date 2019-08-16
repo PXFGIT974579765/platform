921f172398faa74958a125687468c694837f1504
evantre <evantre@qq.com>
2019-08-16 11:23:50 +0800

    feat: share button

--- src/components/ShareButton.vue
+++ src/components/ShareButton.vue
@@ -1,12 +1,40 @@
 <template>
-  <div class="share-button">
-    <span class="iconfont">&#xe748;</span>
-    <span>分享</span>
+  <div>
+    <div class="share-button" @click="onShowShare">
+      <span class="iconfont">&#xe748;</span>
+      <span>分享</span>
+    </div>
+
+    <van-popup
+      v-model="shareShow"
+      position="bottom"
+      :style="{ maxHeight: '75%' }"
+    >
+      <share />
+    </van-popup>
   </div>
 </template>
 
 <script>
-export default {}
+import Share from '@/components/Share'
+
+export default {
+  components: {
+    Share,
+  },
+
+  data() {
+    return {
+      shareShow: false,
+    }
+  },
+
+  methods: {
+    onShowShare() {
+      this.shareShow = true
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
