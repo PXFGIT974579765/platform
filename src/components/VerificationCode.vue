1b1a782a02899f381c7929ad4200526f5a9c5ff3
evantre <evantre@qq.com>
2019-08-17 15:55:26 +0800

    feat: point

---
+++ src/components/VerificationCode.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="verification-code">
+    <van-icon name="cross" :size="16" class="close" @click="onClose" />
+    <div>输入短信验证码</div>
+    <div class="contact">
+      <span>13201356829</span>
+      <span>58</span>
+    </div>
+    <div class="inputs">
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+    },
+  },
+
+  methods: {
+    onClose() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.verification-code {
+  position: relative;
+  text-align: center;
+  padding: 0 20px;
+}
+
+.close {
+  position: absolute;
+  top: 12px;
+  right: 12px;
+  color: #adadad;
+}
+</style>
